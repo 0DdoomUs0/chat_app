b4d26b8051521a5650560f3de15a6b525568f5a2
David Dumas <ddumas@ultramain.com>
2017-10-01 11:42:52 -0600

    additional functionality

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ app.get("/style.css", function(req, res){
 });
 
 io.on('connection', function(socket){
+    io.emit('chat message', 'user connected');
+    var nick = 'anonymous';
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        io.emit('chat message', `${nick}: ${msg}`);
+    });
+    socket.on('set nick', function(new_nick){
+        nick = new_nick;
+    });
+    socket.on('disconnect', () => {
+        io.emit('chat message', `${nick} disconnected`);
     });
 });
 
