110b6dd8e102960af80607f3d3fb16ada6573c12
David Dumas <ddumas@ultramain.com>
2017-09-30 20:03:43 -0600

    finishes up chat emittission

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ app.get("/style.css", function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
     });
-})
+});
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
