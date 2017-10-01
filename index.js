d513f84ef07292b97ac2f7b7e8bb914544d7b038
David Dumas <ddumas@ultramain.com>
2017-09-30 19:51:56 -0600

    adds disconnect logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -8,6 +9,13 @@ app.get("/", function(req, res){
 app.get("/style.css", function(req, res){
     res.sendFile(__dirname + '/style.css');
 });
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+})
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
